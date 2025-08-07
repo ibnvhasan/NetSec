8a528442b3eaf4e3422cf8b2916f5f0e888b91f6
abdullah <136981949+ibnvhasan@users.noreply.github.com>
2025-08-07 22:58:21 +0000

    add workshops

--- book/toc.js
+++ book/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="introduction.html">Introduction</a></li><li class="chapter-item expanded affix "><li class="part-title">Applied Classes</li><li class="chapter-item expanded "><a href="applied/week1.html"><strong aria-hidden="true">1.</strong> Week 1</a></li><li class="chapter-item expanded "><a href="applied/week2.html"><strong aria-hidden="true">2.</strong> Week 2</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="introduction.html">Introduction</a></li><li class="chapter-item expanded affix "><li class="part-title">Applied Classes</li><li class="chapter-item expanded "><a href="applied/week1.html"><strong aria-hidden="true">1.</strong> Week 1</a></li><li class="chapter-item expanded "><a href="applied/week2.html"><strong aria-hidden="true">2.</strong> Week 2</a></li><li class="chapter-item expanded affix "><li class="part-title">Workshops</li><li class="chapter-item expanded "><a href="workshops/workshop1.html"><strong aria-hidden="true">3.</strong> Workshop 1</a></li><li class="chapter-item expanded "><a href="workshops/workshop2.html"><strong aria-hidden="true">4.</strong> Workshop 2</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
--- docs/SUMMARY.md
+++ docs/SUMMARY.md
@@ -5,3 +5,7 @@
 # Applied Classes
 - [Week 1](applied/week1.md))
 - [Week 2](applied/week2.md)
+
+# Workshops
+- [Workshop 1](workshops/workshop1.md)
+- [Workshop 2](workshops/workshop2.md)
